95a6831a5cebcfc2dcd93fd5256a08d8dc595c85
RobertWei3 <yufanwei927@gmail.com>
2025-03-20 11:53:34 -0400

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = 'Robert Wei',
+    author_email = 'yufanwei927@gmail.com',
+    description='A generative AI project for text processing',
+    packages = find_packages(),
+    install_requires = []
+)
